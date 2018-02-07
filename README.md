650a43fa078f4211b3714b11ad4e0cae5e3192cd
mrkraimer <mrkraimer@comcast.net>
2018-02-07 15:13:25 -0500

    code similar to  pvAccessCPP/examples/monitorme.cpp and exampleCPP/exampleClient/src/monitor.cpp

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # testClientCPP
 
-This contains tests to show problems in the C++ code for EPICS V4.
+This contains code similar to:
+
+1) pvAccessCPP/examples/monitorme.cpp
+The example does not trap signals and does not use a WorkQueue.
+It terminates when the enter key is pressed.
+
+2) exampleCPP/exampleClient/src/monitor.cpp
+The example terminates when the enter key is pressed.
+It produces output like monitorme rather that like the original monitor.
+
+In addition this project has code named monitormerate and monitorrate.
+These are used to measure performance in events per second.
+
 
 ## Building
 
@@ -16,6 +28,29 @@ It can also be built by:
 
 In **configure/RELEASE.local** it may only be necessary to change the definitions
 of **EPICS4_DIR** and **EPICS_BASE**.
+
+## Starting IOCs for test
+
+There are files named fast1.db, ..., fast4.db.
+These are databases for use with **softIocPVA** that is provided with pvAccessCPP.
+This creates an IOC that has DBRecords and also provider **qsrv**.
+
+To use these a softlink can be created to **softIocPVA**.
+For example I created  soft link as follows:
+
+    ln -s /home/epicsv4/masterCPP/pva2pva/bin/linux-x86_64/softIocPVA softIocPVA
+
+Then each can be used to create an IOC.
+For example:
+
+     ./softIocPVA -d fast1.db
+
+Similarly IOCs using fast2.db, fast3.db, and fast4.db can be started.
+
+## Performance
+
+File performResult shows performance results.
+
 
 
 
--- src/monitormerate.cpp
+++ src/monitormerate.cpp
@@ -34,7 +34,86 @@ namespace {
 typedef epicsGuard<epicsMutex> Guard;
 typedef epicsGuardRelease<epicsMutex> UnGuard;
 
+struct Worker {
+    virtual ~Worker() {}
+    virtual void process(const pvac::MonitorEvent& event) =0;
+};
+
+// simple work queue with thread.
+// moves monitor queue handling off of PVA thread(s)
+struct WorkQueue : public epicsThreadRunable {
+    epicsMutex mutex;
+    typedef std::tr1::shared_ptr<Worker> value_type;
+    typedef std::tr1::weak_ptr<Worker> weak_type;
+    // work queue holds only weak_ptr
+    // so jobs must be kept alive seperately
+    typedef std::deque<std::pair<weak_type, pvac::MonitorEvent> > queue_t;
+    queue_t queue;
+    epicsEvent event;
+    bool running;
+    pvd::Thread worker;
+
+    WorkQueue()
+        :running(true)
+        ,worker(pvd::Thread::Config()
+                .name("Monitor handler")
+                .autostart(true)
+                .run(this))
+    {}
+    ~WorkQueue() {close();}
+
+    void close()
+    {
+        {
+            Guard G(mutex);
+            running = false;
+        }
+        event.signal();
+        worker.exitWait();
+    }
+
+    void push(const weak_type& cb, const pvac::MonitorEvent& evt)
+    {
+        bool wake;
+        {
+            Guard G(mutex);
+            if(!running) return; // silently refuse to queue during/after close()
+            wake = queue.empty();
+            queue.push_back(std::make_pair(cb, evt));
+        }
+        if(wake)
+            event.signal();
+    }
+
+    virtual void run() OVERRIDE FINAL
+    {
+        Guard G(mutex);
+
+        while(running) {
+            if(queue.empty()) {
+                UnGuard U(G);
+                event.wait();
+            } else {
+                queue_t::value_type ent(queue.front());
+                value_type cb(ent.first.lock());
+                queue.pop_front();
+                if(!cb) continue;
+
+                try {
+                    UnGuard U(G);
+                    cb->process(ent.second);
+                }catch(std::exception& e){
+                    std::cout<<"Error in monitor handler : "<<e.what()<<"\n";
+                }
+            }
+        }
+    }
+};
+
+WorkQueue monwork;
+
 struct MonTracker : public pvac::ClientChannel::MonitorCallback,
+                    public Worker,
                     public std::tr1::enable_shared_from_this<MonTracker>
 {
     POINTER_DEFINITIONS(MonTracker);
@@ -52,6 +131,7 @@ struct MonTracker : public pvac::ClientChannel::MonitorCallback,
     volatile long numMonitor;
     TimeStamp timeStamp;
     TimeStamp timeStampLast;
+    epicsMutex mutex;
 
     virtual void monitorEvent(const pvac::MonitorEvent& evt) OVERRIDE FINAL
     {
@@ -61,32 +141,58 @@ struct MonTracker : public pvac::ClientChannel::MonitorCallback,
         // running on internal provider worker thread
         // minimize work here.
         // TODO: bound queue size
-       // monwork.push(shared_from_this(), evt);
-        switch(evt.event) {
-            case pvac::MonitorEvent::Fail:
-                std::cout<<"Error "<<name<<" "<<evt.message<<"\n";
-                break;
-            case pvac::MonitorEvent::Cancel:
-                std::cout<<"Cancel "<<name<<"\n";
-                break;
-            case pvac::MonitorEvent::Disconnect:
-                std::cout<<"Disconnect "<<name<<"\n";
-                break;
-            case pvac::MonitorEvent::Data:
-            {
-                unsigned n;
-                for(n=0; n<2 && mon.poll(); n++)
-                {
-                    pvd::PVField::const_shared_pointer fld(mon.root->getSubField("value"));
-                    if(!fld)
-                        fld = mon.root;
+        monwork.push(shared_from_this(), evt);
+    }
 
-                    std::cout<<"Event "<<name<<" "<<fld
-                        <<" Changed:"<<mon.changed
-                        <<" overrun:"<<mon.overrun<<"\n";
-                }
+    virtual void process(const pvac::MonitorEvent& evt) OVERRIDE FINAL
+    {
+        // running on our worker thread
+        switch(evt.event) {
+        case pvac::MonitorEvent::Fail:
+            std::cout<<"Error "<<name<<" "<<evt.message<<"\n";
+            break;
+        case pvac::MonitorEvent::Cancel:
+            std::cout<<"Cancel "<<name<<"\n";
+            break;
+        case pvac::MonitorEvent::Disconnect:
+            std::cout<<"Disconnect "<<name<<"\n";
+            break;
+        case pvac::MonitorEvent::Data:
+        {
+            unsigned n;
+            for(n=0; n<2 && mon.poll(); n++) {
+            {
+                Guard G(mutex);
+                numMonitor++;
+            }
+            }
+            if(n==2) {
+                // too many updates, re-queue to balance with others
+                monwork.push(shared_from_this(), evt);
             }
         }
+            break;
+        }
+    }
+
+    double report()
+    {
+        timeStamp.getCurrent();
+        double  diff = TimeStamp::diff(timeStamp,timeStampLast);
+        long numnow = 0;
+        {
+            Guard G(mutex);
+            numnow = numMonitor;
+            numMonitor = 0;
+        }
+        timeStampLast.getCurrent();
+        double persecond = 0;
+        if(diff>0.0) persecond = numnow/diff;
+        cout << name
+        << " numnow " << numnow
+        << " persecond " << persecond
+        << endl;
+        return persecond;
     }
 
 };
@@ -154,9 +260,18 @@ int main(int argc, char *argv[]) {
             monitors.push_back(mon);
         }
         int ret = 0;
-        string str;
-        getline(cin,str);
-cout << "after getline\n";
+        while(true) {
+            string str;
+            getline(cin,str);
+            if(str.compare("exit")==0){
+                 break;
+            }
+            double events = 0.0;
+            for(size_t i=0; i<monitors.size(); ++i) {
+                events += monitors[i]->report();
+            }
+            cout << "total events/second " << events << endl;
+        }
         if(refmon.running()) {
             refmon.stop();
             // drop refs to operations, but keep ref to ClientProvider
@@ -164,6 +279,8 @@ cout << "after getline\n";
             // show final counts
             refmon.current();
         }
+cout << "calling monwork.close\n";
+        monwork.close();
 cout << "calling monitors.clear\n";
 monitors.clear();
 cout << "returning\n";
